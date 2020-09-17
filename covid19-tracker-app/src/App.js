0ba6813eb62dc02732fc3be29baa1569200932c8
Yash Shah <yash.shah.2242000@gmail.com>
2020-09-18 00:59:23 +0530

    Figures Shown

--- covid19-tracker-app/src/App.js
+++ covid19-tracker-app/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker} from './components';
 import styles from '../src/App.module.css';
+import { fetchData } from './api';
 
 
 class App extends Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData })
+  }
   render() {
+
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
